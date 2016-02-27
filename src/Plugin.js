77a852c97cbb25bf96d8bb3b8b7711cd3e2e19b4
Zlatko Fedor <zfedor@gmail.com>
2016-02-27 16:57:14 +0100

    Release 0.1.4

--- src/Plugin.js
+++ src/Plugin.js
@@ -1,6 +1,10 @@
+import merge from 'lodash/merge';
+import fs from 'fs';
+
 export default class ExportLocales {
   constructor(options = {}) {
     this._options = options;
+    this._locales = {};
 
     this.apply = this.apply.bind(this);
     this.addExtractedLocale = this.addExtractedLocale.bind(this);
@@ -10,10 +14,40 @@ export default class ExportLocales {
     return this._options;
   }
 
-  addExtractedLocale(locales, propertyName) {
-    const options = this.getOptions();
+  addExtractedLocale(extractedLocales, propertyName) {
+    const locales = this._locales;
 
-    console.log(propertyName, locales);
+    Object.keys(extractedLocales).forEach((locale) => {
+      const extracted = extractedLocales[locale];
+      if (!locales[locale]) {
+        locales[locale] = {};
+      }
+
+      const currentLocale = locales[locale];
+      currentLocale[propertyName] = extracted;
+    });
+
+    this.save();
+  }
+
+  save() {
+    const options = this.getOptions();
+    const locales = this._locales;
+
+    const mainDir = options.path || '.';
+
+    Object.keys(locales).forEach((locale) => {
+      const filePath = mainDir + '/' + locale + '.js';
+
+      const jsonContent = JSON.stringify(locales[locale], undefined, '\t');
+      const result = `module.exports = ${jsonContent};`;
+
+      fs.writeFile(filePath, result, { flag: 'w+' }, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
   }
 
   apply(compiler) {
