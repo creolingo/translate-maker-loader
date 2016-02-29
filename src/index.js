a69de24327edf4c34f24abec084deff4a049898d
Zlatko Fedor <zfedor@gmail.com>
2016-02-29 12:57:09 +0100

    Release 0.2.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import loader from './loader';
 import ExtractLocales, { Format } from './Plugin';
+import ResponseFormat from './constants/ResponseFormat';
 
-export { ExtractLocales, Format };
+export { ExtractLocales, Format, ResponseFormat };
 export default loader;
 
 // commonjs
 loader.ExtractLocales = ExtractLocales;
 loader.Format = Format;
+loader.ResponseFormat = ResponseFormat;
+
 module.exports = loader;
